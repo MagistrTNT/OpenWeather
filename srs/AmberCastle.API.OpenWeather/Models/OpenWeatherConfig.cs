316a2df3911c1d5e521287a6db4e375e82f90ee0
Magistr <MagistrTNT@msn.com>
2025-02-08 01:59:38 +0300

    Сусальность для хранения конфигурации API

---
+++ srs/AmberCastle.API.OpenWeather/Models/OpenWeatherConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmberCastle.API.OpenWeather.Models
+{
+    public class OpenWeatherConfig
+    {
+        public string ApiKey { get; init; } = default!;
+        public Uri OpenWeatherUrl { get; init; } = new Uri("http://api.openweathermap.org");
+        public string Units { get; init; } = "metric";
+        public string Lang { get; init; } = "ru";
+    }
+}
